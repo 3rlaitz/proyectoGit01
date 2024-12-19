763e59dda0a2384fb3bea3b030d8b9184c09a616
Erlaitz Alonso <erlaitz.alonso05@somo.com>
2024-12-19 11:34:40 +0100

    Añadidos otros constructores y GETers & SETers

--- src/paqueteGIT/Empleado.java
+++ src/paqueteGIT/Empleado.java
@@ -1,14 +1,19 @@
 package paqueteGIT;
 
+public class Empleado {
+
 	private String dni;
 	private String nombre;
 	private String apellidos;
 	private byte edad;
 	private float sueldo;
 	private boolean hijos; //True si tiene hijos/as. False lo contrario
+	private double ayuda; //Ayuda para empleados en situación de necesidad
 	
-	////CONSTRUCTORES////
-	public class Empleado {
+	
+	/******** CONSTRUCTORES *********/
+	
+	////// CONSTRUCTOR COMPLETO ///////
 		public Empleado(String dni, String nombre, String apellidos, byte edad, float sueldo, boolean hijos) {
 			super();
 			this.dni = dni;
@@ -17,9 +22,108 @@ package paqueteGIT;
 			this.edad = edad;
 			this.sueldo = sueldo;
 			this.hijos = hijos;
+			this.ayuda = (hijos)?sueldo*0.10:20;
 		}
 		
 		
+		/////// CONSTRUCTOR VACIO /////////
+			public Empleado() {
+				super();
+				this.dni = "";
+				this.nombre = "********";
+				this.apellidos = "******";
+				this.edad = 0;
+				this.sueldo = (float) 0.0;
+				this.hijos = false;
+				this.ayuda = (hijos)?sueldo*0.10:20;
+			}
+			
 		
+		///////// CONSTRUCTOR PARCIAL (solo DNI y NOMBRE) /////////
+		public Empleado(String dni, String nombre) {
+			super();
+			this.dni = dni;
+			this.nombre = nombre;
+			this.apellidos = "********";
+			this.edad = 0;
+			this.sueldo = (float)0.0;
+			this.hijos = false;
+			this.ayuda = (hijos)?sueldo*0.10:20;
+		}
+		
+		
+		//////////// GETers & SETers //////////
+
+		public String getDni() {
+			return dni;
+		}
+
+
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+
+
+		public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public String getApellidos() {
+			return apellidos;
+		}
+
+
+		public void setApellidos(String apellidos) {
+			this.apellidos = apellidos;
+		}
+
+
+		public byte getEdad() {
+			return edad;
+		}
+
+
+		public void setEdad(byte edad) {
+			this.edad = edad;
+		}
+
+
+		public float getSueldo() {
+			return sueldo;
+		}
+
+
+		public void setSueldo(float sueldo) {
+			this.sueldo = sueldo;
+		}
+
+
+		public boolean isHijos() {
+			return hijos;
+		}
+
+
+		public void setHijos(boolean hijos) {
+			this.hijos = hijos;
+		}
+
+
+		public double getAyuda() {
+			return ayuda;
+		}
+
+
+		public void setAyuda() {
+			this.ayuda = (hijos)?sueldo*0.10:20;
+		}
+		
+		
+
 }
 
